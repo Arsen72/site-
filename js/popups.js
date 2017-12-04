a5c8023762f606099d794013eecddb2cc56eed51
Arsen <palamarchuk.arsen@yandex.ru>
2017-12-04 21:07:19 +0200

    project

---
+++ js/popups.js
@@ -0,0 +1,21 @@
+$('.exit').click( function () {
+	$('.w-fon').show();
+	$('.window-exit').show();
+});
+
+$('#exit-close').click( function () {
+	$('.w-fon').hide();
+	$('.window-exit').hide();
+});
+
+
+
+$('.reg').click( function () {
+	$('.w-fon').show();
+	$('.window-reg').show();
+});
+
+$('#reg-close').click( function () {
+	$('.w-fon').hide();
+	$('.window-reg').hide();
+});
---
+++ js/sl-script.js
@@ -0,0 +1,12 @@
+$('.main-slider').slick({
+	autoplay: true,
+	autoplaySpeed: 3000,
+	fade: true,
+	dots: true,
+	arrows: false
+});
+
+$('.second-slider').slick({
+	 slidesToShow: 4,
+ 	 slidesToScroll: 1
+});
